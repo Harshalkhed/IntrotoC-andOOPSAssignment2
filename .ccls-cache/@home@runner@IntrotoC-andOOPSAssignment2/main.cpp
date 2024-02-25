9fc9c768bd959845eed2d576b3bb40e592b4523a
Harshal S Khedekar <37449892+Harshalkhed@users.noreply.github.com>
2024-02-25 06:15:27 +0000

    Change

--- .ccls-cache/@home@runner@IntrotoC-andOOPSAssignment2/main.cpp
+++ .ccls-cache/@home@runner@IntrotoC-andOOPSAssignment2/main.cpp
@@ -8,10 +8,84 @@ Implement cool functionalities in each enemy class, e.g implement the Fly() func
   */
 #include <iostream>
 using namespace std;
+class Enemy {
+protected:
+    int health;
+    int attackPower;
+    int attackRange;
+    int XP;
+    int Level;
 
+public:
+    // Constructor
+    Enemy(int health, int attackPower, int attackRange, int XP, int Level)
+        : health(health), attackPower(attackPower), attackRange(attackRange), XP(XP), Level(Level) {}
 
+    // Getter methods
+    int getHealth() const { return health; }
+    int getAttackPower() const { return attackPower; }
+    int getAttackRange() const { return attackRange; }
+    int getXP() const { return XP; }
+    int getLevel() const { return Level; }
 
+    // Setter methods
+    void setHealth(int h) { health = h; }
+    void setAttackPower(int ap) { attackPower = ap; }
+    void setAttackRange(int ar) { attackRange = ar; }
+    void setXP(int xp) { XP = xp; }
+    void setLevel(int lvl) { Level = lvl; }
+};
+
+class FlyingEnemy : public Enemy {
+private:
+    int flyingRadius;
+
+public:
+    // Constructor
+    FlyingEnemy(int health, int attackPower, int attackRange, int XP, int Level, int flyingRadius)
+        : Enemy(health, attackPower, attackRange, XP, Level), flyingRadius(flyingRadius) {}
+
+    // Getter method
+    int getFlyingRadius() const { return flyingRadius; }
+
+    // Setter method
+    void setFlyingRadius(int fr) { flyingRadius = fr; }
+
+    // Functionality for flying enemy
+    void fly() {
+        std::cout << "I'm flying around gracefully!" << std::endl;
+    }
+};
+
+class GroundEnemy : public Enemy {
+private:
+    int patrollingRadius;
+
+public:
+    // Constructor
+    GroundEnemy(int health, int attackPower, int attackRange, int XP, int Level, int patrollingRadius)
+        : Enemy(health, attackPower, attackRange, XP, Level), patrollingRadius(patrollingRadius) {}
+
+    // Getter method
+    int getPatrollingRadius() const { return patrollingRadius; }
+
+    // Setter method
+    void setPatrollingRadius(int pr) { patrollingRadius = pr; }
+
+    // Functionality for ground enemy
+    void patrol() {
+         cout << "I'm patrolling the area, looking for intruders..." << std::endl;
+    }
+};
 
 int main() {
-  std::cout << "Hello World!\n";
+    FlyingEnemy flyingEnemy(100, 20, 10, 50, 5, 30);
+    cout << "Flying Enemy Health: " << flyingEnemy.getHealth() << std::endl;
+    flyingEnemy.fly();
+
+    GroundEnemy groundEnemy(150, 30, 15, 75, 6, 50);
+    cout << "Ground Enemy Health: " << groundEnemy.getHealth() << std::endl;
+    groundEnemy.patrol();
+
+    return 0;
 }
